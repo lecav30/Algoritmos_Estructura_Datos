5e85041499bc6f363e511af29b032d74530ff893
Sebastián Lévano <79949221+lecav30@users.noreply.github.com>
2021-10-16 11:58:39 -0500

    Add files via upload

--- CPLEDE_O/Ordenamientos.h
+++ CPLEDE_O/Ordenamientos.h
@@ -1,10 +1,12 @@
+#pragma once
 #include <functional>
-using std::function;
-
+using namespace std;
 typedef unsigned long long ull;
+typedef long long ll;
+const int RUN = 32;
 
 template <typename T>
-void cambio(T&a, T&b)
+void cambio(T &a, T &b)
 {
     T aux = a;
     a = b;
@@ -12,51 +14,52 @@ void cambio(T&a, T&b)
 }
 
 template <typename T>
-void selectionSort(T arr[], ull n)
+void selectionSort(T *arr, ull n)
 {
     size_t pos;
-    for(size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
     {
         pos = i;
         for (size_t j = i; j < n; ++j)
-            if(arr[j] < arr[pos])
+            if (arr[j] < arr[pos])
                 pos = j;
         cambio(arr[pos], arr[i]);
     }
 }
 
 template <typename T>
-void exchangeSort(T arr[], ull n)
+void exchangeSort(T *arr, ull n)
 {
-    for(size_t i = 0; i < n - 1; ++i)
+    for (size_t i = 0; i < n - 1; ++i)
         for (size_t j = i + 1; j < n; ++j)
-            if(arr[i] > arr[j])
+            if (arr[i] > arr[j])
                 cambio(arr[j], arr[i]);
 }
 
 template <typename T>
-void bubbleSort(T arr[], ull n)
+void bubbleSort(T *arr, ull n)
 {
-    for(size_t i = 0; i < n; ++i)
+    for (size_t i = 0; i < n; ++i)
         for (size_t j = 0; j < n - 1 - i; ++j)
-            if(arr[j] > arr[j + 1])
+            if (arr[j] > arr[j + 1])
                 cambio(arr[j], arr[j + 1]);
 }
 
 template <typename T>
-void shellSort(T arr[], ull n)
+void shellSort(T *arr, ull n)
 {
-    ull salto = n/2, i, j, k;
+    ull salto = n / 2, i, k;
+    ll j;
 
-    while (salto > 0) 
+    while (salto > 0)
     {
         for (i = salto; i < n; i++)
         {
             j = i - salto;
-            while (j >= 0) 
+            while (j >= 0)
             {
                 k = j + salto;
-                if (arr[j] < arr[k])
+                if (arr[j] <= arr[k])
                     j = -1;
                 else
                 {
@@ -65,17 +68,17 @@ void shellSort(T arr[], ull n)
                 }
             }
         }
-        salto /= 2;
+        salto = salto / 2;
     }
 }
 
 template <typename T>
-void insertionSort(T arr[], ull n)
+void insertionSort(T *arr, ull n)
 {
-    ull j;
+    ll j;
     T aux;
 
-    for (size_t i = 1; i < n; i++) 
+    for (size_t i = 1; i < n; i++)
     {
         aux = arr[i];
         j = i - 1;
@@ -89,7 +92,7 @@ void insertionSort(T arr[], ull n)
 }
 
 template <typename T>
-void merge(T arr1[], T arr2[], T arr[], ull n)
+void merge(T *arr1, T *arr2, T *arr, ull n)
 {
     ull i, j, k;
     i = j = k = 0;
@@ -122,14 +125,14 @@ void merge(T arr1[], T arr2[], T arr[], ull n)
         k++;
     }
 }
- 
+
 template <typename T>
-void mergeSort(T arr[], ull n)
+void mergeSort(T *arr, ull n)
 {
     if (n > 1)
     {
-        T* arr1 = new T[n / 2];
-        T* arr2 = new T[n - n / 2];
+        T *arr1 = new T[n / 2];
+        T *arr2 = new T[n - n / 2];
 
         for (size_t i = 0; i < n / 2; ++i)
             arr1[i] = arr[i];
@@ -143,3 +146,120 @@ void mergeSort(T arr[], ull n)
     }
 }
 
+template <typename T>
+ull particion(T *arr, ull p, ull r)
+{
+    T x = arr[r];
+    ull i = p - 1;
+    for (size_t j = p; j < r; j++)
+    {
+        if (arr[j] <= x)
+        {
+            i++;
+            cambio(arr[i], arr[j]);
+        }
+    }
+    cambio(arr[i + 1], arr[r]);
+    return i + 1;
+}
+
+template <typename T>
+void quickSort(T *arr, ull p, ull r)
+{
+    if (p < r)
+    {
+        ull q = particion(arr, p, r);
+        quickSort(arr, p, q - 1);
+        quickSort(arr, q + 1, r);
+    }
+}
+
+template <typename T>
+void insertionT(T *arr, ull izq, ull der)
+{
+    for (size_t i = izq + 1; i <= der; i++)
+    {
+        T temp = arr[i];
+        ull j = i - 1;
+        while (j >= izq && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+template <typename T>
+void mergeT(T *arr, ull l, ull m, ull r)
+{
+    ull c1 = m - l + 1;
+    ull c2 = r - m;
+    T *izq = new T[c1];
+    T *der = new T[c2];
+
+    for (size_t i = 0; i < c1; i++)
+        izq[i] = arr[l + i];
+    for (size_t i = 0; i < c2; i++)
+        der[i] = arr[m + 1 + i];
+
+    ull i = 0;
+    ull j = 0;
+    ull k = l;
+
+    while (i < c1 && j < c2)
+    {
+        if (izq[i] <= der[j])
+        {
+            arr[k] = izq[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = der[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < c1)
+    {
+        arr[k] = izq[i];
+        k++;
+        i++;
+    }
+
+    while (j < c2)
+    {
+        arr[k] = der[j];
+        k++;
+        j++;
+    }
+
+    delete izq;
+    delete der;
+}
+
+template <typename T>
+void timSort(T *arr, ull n)
+{
+    auto min = [](T a, T b) -> T
+    {
+        return a <= b ? a : b;
+    };
+
+    for (size_t i = 0; i < n; i += RUN)
+        insertionT(arr, i, min((i + RUN - 1), (n - 1)));
+
+    for (size_t size = RUN; size < n; size = 2 * size)
+    {
+        for (size_t izq = 0; izq < n; izq += 2 * size)
+        {
+            ull mid = izq + size - 1;
+            ull der = min((izq + 2 * size - 1), (n - 1));
+
+            if (mid < der)
+                mergeT(arr, izq, mid, der);
+        }
+    }
+}
--- CPLEDE_O/test.cpp
+++ CPLEDE_O/test.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <ctime>
 #include "Ordenamientos.h"
 using namespace std;
 
 template <typename T>
-void printArray(T arr[], unsigned long n)
+void printArray(T *arr, unsigned long n)
 {
     for (size_t i = 0; i < n; i++)
         cout << arr[i] << " ";
@@ -12,21 +13,38 @@ void printArray(T arr[], unsigned long n)
 
 int main()
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    long n = sizeof(arr) / sizeof(arr[0]);
+    srand(time(NULL));
+    /*int arr[] = { 12, 11, 13, 5, 6 };
+    long n = 5;*/
+
+    int *arr = new int[5];
+    for (int i = 0; i < 5; ++i)
+        arr[i] = rand() % 100 + 1;
+    long n = 5;
 
     cout << "Array before sort:\n";
     printArray(arr, n);
 
     //selectionSort(arr, n);
-    //exchangeSort(arr,n);
+    // listo
+    //exchangeSort(arr, n);
+    // listo
     //bubbleSort(arr, n);
+    // listo
     //shellSort(arr, n);
-    //insertionSort(arr, n);
-    mergeSort(arr, n);
+    //listo
+    insertionSort(arr, n);
+    // listo
+    //mergeSort(arr, n);
+    // listo
+    //quickSort(arr, 0, n - 1);
+    // listo
+    //timSort(arr, n);
+    //listo
 
     cout << "\nArray after sort:\n";
     printArray(arr, n);
 
+    system("pause");
     return 0;
 }
